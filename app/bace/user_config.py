f06879304e798cb808365129caf73aaf15ed2689
Jiayue Zhang <74217446+jzhangg@users.noreply.github.com>
2024-07-17 23:28:30 +0300

    Update user_config.py

--- app/bace/user_config.py
+++ app/bace/user_config.py
@@ -48,17 +48,17 @@ def likelihood_pdf(answer, thetas,
     p_b = repay_b * 4 + 24*362
                        
 
-    #if thetas['k']<p_a:
-    U_a = -price_a + 0.5/thetas['vbar']*( (thetas['vbar']-p_a)^2 + 
-                                         (thetas['k']/p_a*thetas['vbar'] - 0.5*(2*p_a-thetas['k']) * thetas['k'] / thetas['vbar'] )^2 )
-    #else:
-    #    U_a = -price_a + 0.5/thetas['vbar']*((thetas['vbar']-p_a)^2 + (thetas['vbar']-0.5*p_a^2/thetas['vbar'])^2 )
+    if thetas['k']<p_a:
+        U_a = -price_a + 0.5/thetas['vbar']*( (thetas['vbar']-p_a)**2 + 
+                                         (thetas['k']/p_a*thetas['vbar'] - 0.5*(2*p_a-thetas['k']) * thetas['k'] / thetas['vbar'] )**2 )
+    else:
+        U_a = -price_a + 0.5/thetas['vbar']*((thetas['vbar']-p_a)**2 + (thetas['vbar']-0.5*p_a^2/thetas['vbar'])**2 )
     
-    #if thetas['k']<p_b:
-    U_b = -price_b + 0.5/thetas['vbar']*( (thetas['vbar']-p_b)^2 + 
-                                         (thetas['k']/p_b*thetas['vbar'] - 0.5*(2*p_b-thetas['k']) * thetas['k'] / thetas['vbar'])^2 )
-    #else:                  
-    #    U_b = -price_b + 0.5/thetas['vbar']*((thetas['vbar']-p_b)^2 + (thetas['vbar']-0.5*p_b^2/thetas['vbar'])^2 )
+    if thetas['k']<p_b:
+    U_b = -price_b + 0.5/thetas['vbar']*( (thetas['vbar']-p_b)**2 + 
+                                         (thetas['k']/p_b*thetas['vbar'] - 0.5*(2*p_b-thetas['k']) * thetas['k'] / thetas['vbar'])**2 )
+    else:                  
+        U_b = -price_b + 0.5/thetas['vbar']*((thetas['vbar']-p_b)**2 + (thetas['vbar']-0.5*p_b^2/thetas['vbar'])**2 )
 
     #U_a = -price_a + thetas['vbar']*repay_a
     #U_b = -price_b + thetas['vbar']*repay_b
